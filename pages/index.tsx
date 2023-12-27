9ed082e92d9bcd853ecbc1b7fbac53b49452cc74
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-12-27 08:44:58 -0300

    Criando projeto com a versaõ 13

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+
+export default function Home() {
+
+  return (   
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5x1 w-full items-center justify-between font-mono text-sm">
+        <h1>Projeto de Bloco</h1>
+     
+      </div>
+   
+    </main>
+    
+  )
+}
