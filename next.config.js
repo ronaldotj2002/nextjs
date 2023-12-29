c34bbd19f2c8bb22d26463aae7c51d485c35b762
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-12-29 20:37:30 -0300

    swc force

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ const nextConfig = {
     config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, fs: false };
     return config;
   },
-  babel: {
-    presets: ['next/babel'], // Configuração do preset do Next.js para o Babel
-    // ... outras configurações do Babel, se necessário
+  experimental: {
+    forceSwcTransforms: true,
   },
 }
 
