ff1ccd670413a2db78313badc8a65650ec072994
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-12-29 20:20:29 -0300

    ajustes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, fs: false };
     return config;
   },
+  babel: {
+    presets: ['next/babel'], // Configuração do preset do Next.js para o Babel
+    // ... outras configurações do Babel, se necessário
+  },
 }
 
 module.exports = nextConfig
