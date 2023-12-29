97c0802982919877ea15c1a33cbb0e86e13e03d7
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-12-29 18:03:07 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: config => {
-    config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false };
+    config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, fs: false };
     return config;
   },
 }
