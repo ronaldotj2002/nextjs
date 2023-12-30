96081c5d85ceec6f2bbe766d4b181d9fbdf5ee78
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2023-12-29 22:05:45 -0300

    teste novo

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   // reactStrictMode: true,
-  webpack: config => {
+  webpack5: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.fallback = { ...config.resolve.fallback, net: false, os: false, fs: false };
+
+    if (!isServer) {
+      config.module.rules.push({
+        test: /\.html$/,
+        use: 'html-loader'
+      });
+    }
+
     return config;
   },
   experimental: {
     forceSwcTransforms: true,
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.html$/,
-  //       use: 'html-loader'
-  //     }
-     
-  //   ]
-  // }
-}
-
-module.exports = nextConfig
+};
