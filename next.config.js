c1d4e19fbb1b96341854a6876a8b194acde5d1f6
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2024-01-03 20:03:27 -0300

    teste..

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
       tls: false,
       dgram: false,
       dns: false,
-      timers: require.resolve('timers/promises'),
+      timers: require?.resolve('timers/promises'),
      };
 
     if (!isServer) {
