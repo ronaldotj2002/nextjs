b6d0f0d812f02a880a1127a289a5d6173dad30b4
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2024-01-03 13:57:37 -0300

    next-config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,4 @@
-module.exports = {
-  // reactStrictMode: true,
-  // webpack5: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.fallback = { 
-      ...config.resolve.fallback, 
-      net: false, 
-      os: false, 
-      fs: false,
-      child_process: false,
-      "mock-aws-s3": false,
-      "aws-sdk": false,
-      nock: false,
-      tls: false,
-      dgram: false,
-      dns: false,
-      timers: require.resolve('timers/promises'),
-     };
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.html$/,
-        use: 'html-loader'
-      });
-    }
-
-    return config;
-  },
-  experimental: {
-    forceSwcTransforms: true,
-  },
-};
+module.exports = nextConfig
