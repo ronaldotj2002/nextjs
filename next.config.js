0d5b324dad5dfacf4898cc324ffe92ddb9534bc9
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2024-01-03 22:29:36 -0300

    config

--- next.config.js
+++ next.config.js
@@ -5,24 +5,7 @@ module.exports = {
   },
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.fallback = { 
-      ...config.resolve.fallback, 
-      fs: false,
-      net: false, 
-      os: false, 
-      child_process: false,
-      "mock-aws-s3": false,
-      "aws-sdk": false,
-      nock: false,
-      tls: false,
-      dgram: false,
-      dns: false,
-      timers: require.resolve('timers/promises'),
-      path: false
-     };
-
     if (!isServer) {
-     
       config.module.rules.push({
         test: /\.html$/,
         use: 'html-loader'
