18dc012a43bd7f51557be8251c0f33e9d4876a78
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2024-01-03 22:46:37 -0300

    teste

--- next.config.js
+++ next.config.js
@@ -1,20 +1,4 @@
-module.exports = {
-  // reactStrictMode: true,
-  future: {
-    webpack5: true,
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Verifica se está no ambiente de desenvolvimento e não é um servidor
-    if (dev && !isServer) {
-      config.node = {
-        fs: 'empty', // Evita erros de 'fs' durante o desenvolvimento
-      };
-    }
-
-    return config;
-  },
-  experimental: {
-    forceSwcTransforms: true,
-  },
-};
+module.exports = nextConfig
