9d37695831797e386759d273e29ad66d9b837ca7
Ronaldo Teixeira <ronaldotj2002@yahoo.com.br>
2024-01-03 23:06:08 -0300

    next-config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // reactStrictMode: true,
   // webpack5: true,
-  timers: require.resolve('timers/promises'),
+  timers: false,
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.fallback = { 
       ...config.resolve.fallback, 
